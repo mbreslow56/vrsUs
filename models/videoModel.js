2ea74dc634073405e549301a5babd0ebca09351e
Arik92 <arik92@gmail.com>
2017-04-25 14:07:32 +0300

    added models for data types and data flow

---
+++ models/videoModel.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var videoSchema = new Schema {
+  url: String,
+  name: String,
+  description: String
+};
+
+var video = mongoose.model("Video", videoSchema);
+module.exports = video;
