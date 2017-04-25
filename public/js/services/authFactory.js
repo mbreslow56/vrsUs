5be086793b5d04581f368573ff8222448fd738c1
mbreslow56 <mbreslow56@gmail.com>
2017-04-25 15:42:53 +0300

    added ctrls, templates, routes, server, app

---
+++ public/js/services/authFactory.js
@@ -0,0 +1,36 @@
+app.factory('authFactory', function($http){
+  var auth = {};
+
+  auth.currentUser = {};
+  
+  auth.join = function(user) {
+  	return $http.post('/users/join', user)
+  	  .then(function(response){
+  	  	auth.currentUser.username = angular.copy(response.data)
+  	  });
+  };
+  
+  auth.login = function(user) {
+  	return $http.post('/users/login', user)
+  	  .then(function(response){
+  	  	console.log(response.data)
+  	  	auth.currentUser.username = angular.copy(response.data)
+  	  });
+  };  
+
+  auth.getCurrentUser = function() {
+  	return $http.get('/users/currentUser')
+  	  .then(function(response){
+  	  	auth.currentUser.username = angular.copy(response.data)
+  	  })
+  }
+
+  auth.logout = function(user) {
+  	return $http.get('/users/logout')
+  	  .then(function(reponse) {
+  	  	auth.currentUser.username = null;
+  	  })
+  }
+  
+  return auth;
+});
---
+++ server.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var expressSession = require('express-session');
+var bodyParser = require('body-parser');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongoose = require('mongoose');
+var userRoutes = require('./routes/userRoutes');
+var User = require("./models/UserModel");
+
+mongoose.connect('mongodb://localhost/voutr');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//Configure passport and session middleware
+app.use(expressSession({ 
+  secret: 'thisIsASecret', 
+  resave: false, 
+  saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static('public'));
+app.use(express.static('node_modules'));
+
+// Configure passport-local to use user model for authentication
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//This tells the server that when a request comes into '/beers'
+//that it should use the routes in 'beerRoutes'
+//and those are in our new beerRoutes.js file
+app.use('/users', userRoutes);
+
+// main error handler
+// warning - not for use in production code!
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.send({
+    message: err.message,
+    error: err
+  });
+});
+
+app.listen(8008, function() {
+  console.log("8008. voUtr bitch!");
+})
+
+app.all('*', function(req, res) {
+  res.sendFile(__dirname + "/public/index.html")
+});
