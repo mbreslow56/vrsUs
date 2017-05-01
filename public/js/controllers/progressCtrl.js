ba5e056ba5b7f971e657b2768c3a5ef73629f9ba
mbreslow56 <mbreslow56@gmail.com>
2017-05-01 17:34:59 +0300

    social media share buttons and continuing progress bar

--- public/js/controllers/progressCtrl.js
+++ public/js/controllers/progressCtrl.js
@@ -2,38 +2,38 @@
 // // Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
 // app.controller('progressCtrl', ['$scope', function($scope){
 
-// var bar = new ProgressBar.Line(progress, {
-//   strokeWidth: 4,
-//   easing: 'easeInOut',
-//   duration: 1400,
-//   color: '#FFEA82',
-//   trailColor: '#eee',
-//   trailWidth: 1,
-//   svgStyle: {width: '100%', height: '100%'},
-//   text: {
-//     style: {
-//       // Text color.
-//       // Default: same as stroke color (options.color)
-//       color: '#999',
-//       position: 'absolute',
-//       right: '0',
-//       top: '30px',
-//       padding: 0,
-//       margin: 0,
-//       transform: null
+//   var bar = new ProgressBar.Line(progress, {
+//     strokeWidth: 4,
+//     easing: 'easeInOut',
+//     duration: 1400,
+//     color: '#FFEA82',
+//     trailColor: '#eee',
+//     trailWidth: 1,
+//     svgStyle: {width: '100%', height: '100%'},
+//     text: {
+//       style: {
+//         // Text color.
+//         // Default: same as stroke color (options.color)
+//         color: '#999',
+//         position: 'absolute',
+//         right: '0',
+//         top: '30px',
+//         padding: 0,
+//         margin: 0,
+//         transform: null
+//       },
+//       autoStyleContainer: false
 //     },
-//     autoStyleContainer: false
-//   },
-//   from: {color: '#FFEA82'},
-//   to: {color: '#ED6A5A'},
-//   step: (state, bar) => {
-//     bar.setText(Math.round(bar.value() * 100) + ' %');
+//     from: {color: '#FFEA82'},
+//     to: {color: '#ED6A5A'},
+//     step: (state, bar) => {
+//       bar.setText(Math.round(bar.value() * 100) + ' %');
 
 
-//   }
-// });
-// console.log(video1Votes, voteLimit)
-// bar.animate(0.9);  // Number from 0.0 to 1.0
+//     }
+//   });
+//   console.log(video1Votes, voteLimit)
+//   bar.animate(0.9);  // Number from 0.0 to 1.0
 // }]);
 
 app.controller('progressCtrl', ['$scope', function($scope) {
